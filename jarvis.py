e88c420fc014171a81a34e1fd8437a93aa53ebae
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-09 01:00:33 +0330

    Developed a voice enine

--- jarvis.py
+++ jarvis.py
@@ -1,6 +1,10 @@
 import speech_recognition as sr
+import pyttsx3
 
 r = sr.Recognizer()
+
+voice_engine = pyttsx3.init()
+voice_engine.say("Hey, I am Jarvis. I say what you say back to you.")
 
 with sr.Microphone() as source:
 	print("Listening...")
@@ -11,6 +15,8 @@ with sr.Microphone() as source:
 		print("Recognizing...")
 		query = r.recognize_google(audio)
 		print(query)
+		voice_engine.say(query)
+		voice_engine.runAndWait()
 	except Exception as e:
 		print(e)
 		print("404!")
