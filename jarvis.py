3e4cbe3309274a9b2855205c4e499643c6cfe931
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-09 12:50:56 +0330

    Refactored

--- jarvis.py
+++ jarvis.py
@@ -2,6 +2,38 @@ from datetime import datetime
 import speech_recognition as sr
 import pyttsx3
 
+class Jarvis:
+	def __init__(self, voice_recognizer, voice_engine): 
+		self.voice_recognizer = sr.Recognizer() 
+		self.voice_recognizer = pyttsx3.init() 
+
+	def initial_speak(self): 
+		now = datetime.now() 
+		if now.hour == 12:
+			self.voice_engine.say("Hey, Good afternoon! I am Jarvis. I say what you say back to you.")
+		elif now.hour > 12:
+			self.voice_engine.say("Hey, Good evening! I am Jarvis. I say what you say back to you.") 
+
+		self.voice_engine.runAndWait()
+
+	def talk_back(self): 
+		with sr.Microphone() as source:
+			print("Listening...")
+			r.pause_threshold = 1
+			audio = self.voice_recognizer.listen(source)
+
+			try:
+				print("Recognizing...")
+				query = self.voice_recognizer.recognize_google(audio)
+				print("You said: " + query)
+				self.voice_engine.say(query)
+				self.voice_engine.runAndWait()
+			except Exception as e:
+				print(e)
+				self.voice_engine.say("Nothing heard, please try again later")
+				self.voice_engine.runAndWait()
+
+"""
 r = sr.Recognizer()
 now = datetime.now() 
 voice_engine = pyttsx3.init()
@@ -32,3 +64,4 @@ with sr.Microphone() as source:
 	except Exception as e:
 		print(e)
 		print("404!")
+"""
