5e277e339ef1638869f29de858381549ac5a6616
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-09 13:00:03 +0330

    Got rid of the dead code

--- jarvis.py
+++ jarvis.py
@@ -38,36 +38,3 @@ if __name__ == '__main__':
 	jarvis_init = Jarvis(sr.Recognizer(), pyttsx3.init())
 	jarvis_init.initial_speak() 
 	jarvis_init.talk_back()
-
-"""
-r = sr.Recognizer()
-now = datetime.now() 
-voice_engine = pyttsx3.init()
-
-if now.hour == 12:
-	voice_engine.say("Hey, Good afternoon! I am Jarvis. I say what you say back to you.")
-elif now.hour > 12:
-	voice_engine.say("Hey, Good evening! I am Jarvis. I say what you say back to you.")
-	
-voice_engine.runAndWait()
-
-with sr.Microphone() as source:
-	print("Listening...")
-	r.pause_threshold = 1
-	audio = r.listen(source)
-
-	try:
-		print("Recognizing...")
-		query = r.recognize_google(audio)
-		print(query)
-		if query == 'switch to Farsi':
-			voice_engine.setProperty('voice', 'fa')
-			voice_engine.say('سلام، من جارویس هستم. یک دستیار صوتی')
-			voice_engine.runAndWait()
-		else: 
-			voice_engine.say(query)
-			voice_engine.runAndWait()
-	except Exception as e:
-		print(e)
-		print("404!")
-"""
