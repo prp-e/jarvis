b104af011e210dde3ef9c6271a97afb645b84f74
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-12 13:46:20 +0330

    Adding the location to configurations

--- config_example.py
+++ config_example.py
@@ -1,3 +1,4 @@
 WEATHER_API_KEY = "YOUR API KEY" # get one from openweathermap.org
 MY_NAME = "YOUR NAME" # enter your name here. 
 MY_TITLE = "PREFERRED TITLE" # enter your title here. 
+NEIGHBORHOOD = 0
